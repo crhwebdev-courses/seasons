93664ecc999b7fe9da434136ed425c73bc05cf3d
Carl Hain <crhain@gmail.com>
2019-07-15 14:29:35 -0500

    Update App component to show error message if an error occurs; otherwise it just shows latitude

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => console.log(err)
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render() {
+    if (this.state.errorMessage) {
+      return (
+        <div>
+          Latitude: {this.state.lat}
+          <br />
+          Error: {this.state.errorMessage}
+        </div>
+      );
+    }
+
     return <div>Latitude: {this.state.lat}</div>;
   }
 }
