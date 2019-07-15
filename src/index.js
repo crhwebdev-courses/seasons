26ae155e2f62902059eed6438b1f2c26607a609f
Carl Hain <crhain@gmail.com>
2019-07-15 14:35:06 -0500

    Refactor App component to show either latitude, and error message or a loading message depending on what informatino has been recieved

--- src/index.js
+++ src/index.js
@@ -17,17 +17,15 @@ class App extends React.Component {
   }
 
   render() {
-    if (this.state.errorMessage) {
-      return (
-        <div>
-          Latitude: {this.state.lat}
-          <br />
-          Error: {this.state.errorMessage}
-        </div>
-      );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
-    return <div>Latitude: {this.state.lat}</div>;
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
