cf704a191ba86a6df705ade09c85ca57fba1b5c0
Carl Hain <crhain@gmail.com>
2019-08-16 14:49:01 -0500

    Pass lattitude to SeasonDisplay as a prop

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay />;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...</div>;
