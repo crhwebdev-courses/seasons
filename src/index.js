9e5dbb6b73c8375ac19195c59b9ed634d17dd33d
Carl Hain <crhain@gmail.com>
2019-10-26 18:45:30 -0500

    First step in refactoring from class-based to using hooks is to import some React functions into index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
